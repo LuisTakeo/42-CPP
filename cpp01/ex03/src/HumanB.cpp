55739b9d91912119f4dbd013c6eb1a03961321cc
LuisTakeo <luistakeo@hotmail.com>
2024-08-17 17:31:40 -0300

    Fix: mensagem no HumanB ao apontar Weapon para nulo

--- cpp01/ex03/src/HumanB.cpp
+++ cpp01/ex03/src/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: tpaim-yu <tpaim-yu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 22:05:50 by tpaim-yu          #+#    #+#             */
-/*   Updated: 2024/08/14 22:05:50 by tpaim-yu         ###   ########.fr       */
+/*   Updated: 2024/08/17 17:23:22 by tpaim-yu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,10 @@ void	HumanB::setWeapon(Weapon &weapon)
 
 void	HumanB::attack() const
 {
+	if (!this->_weapon)
+	{
+		std::cout << this->_name << " is unnarmed. Oh no!"<< std::endl;
+		return ;
+	}
 	std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
 }
