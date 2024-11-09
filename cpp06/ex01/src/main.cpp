b261ac7a743d42c4838ec56223b6bc5d95e03836
LuisTakeo <luistakeo@hotmail.com>
2024-11-09 20:14:13 -0300

    Feat: Testes realizados

--- cpp06/ex01/src/main.cpp
+++ cpp06/ex01/src/main.cpp
@@ -6,12 +6,34 @@ int main(void)
 	Data		*ptr;
 	uintptr_t	raw;
 
+	//Test 1
+	std::cout << "Test 1: Convert a data structure to a raw data" << std::endl;
 	data.name = "AnteDEGEMON";
 	data.age = 42;
 	std::cout << "Data: " << data.name << " " << data.age << std::endl;
 	raw = Serializer::serialize(&data);
-	std::cout << "Raw: " << raw << std::endl;
+	std::cout << "Result of the serialize, raw: " << raw << std::endl;
+
+	//Test 2
+	std::cout << "Test 2: Convert a raw data to a data structure" << std::endl;
 	ptr = Serializer::deserialize(raw);
-	std::cout << "Data: " << ptr->name << " " << ptr->age << std::endl;
+	std::cout << "Result of deserialize: Name - " << ptr->name << ". Age - " << ptr->age << std::endl;
+
+	//Test 3
+	std::cout << "Test 3: Convert a null pointer to a raw data" << std::endl;
+	Data		*nullToPtr = NULL;
+	raw = Serializer::serialize(nullToPtr);
+	std::cout << "Result of the serialize, raw: " << raw << std::endl;
+
+	//Test 4
+	std::cout << "Test 4: Convert an invalid pointer" << std::endl;
+	Data		*invalidPtr = reinterpret_cast<Data *>(0xdeadc0de);
+	raw = Serializer::serialize(invalidPtr);
+	std::cout << "Result of the serialize, raw: " << raw << std::endl;
+	ptr = Serializer::deserialize(raw);
+	std::cout << "Result of the deserialize, Data: " << ptr << std::endl;
+	// the following line will cause a segmentation fault
+	// std::cout << "Result: Name - " << ptr->name << ". Age - " << ptr->age << std::endl;
+	std::cout << "End!" << std::endl << "Segfault! Ops, kiddin xd" << std::endl;
 	return 0;
 }
