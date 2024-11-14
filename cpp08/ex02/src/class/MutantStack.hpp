c2ec3b6cc47071d2b2c715e81257e7b3e0daa7a3
LuisTakeo <luistakeo@hotmail.com>
2024-11-14 01:40:03 -0300

    Finished 08

--- cpp08/ex02/src/class/MutantStack.hpp
+++ cpp08/ex02/src/class/MutantStack.hpp
@@ -52,6 +52,10 @@ template <typename T>
 typename MutantStack<T>::const_iterator MutantStack<T>::begin() const
 { return std::stack<T>::c.begin(); }
 
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::end() const
+{ return std::stack<T>::c.end(); }
+
 template <typename TContainer>
 void printContainer(TContainer const &container)
 {
