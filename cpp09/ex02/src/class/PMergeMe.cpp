9ebd25526716f7c686d9184ddd2030b46264f66c
LuisTakeo <luistakeo@hotmail.com>
2024-11-16 19:03:33 -0300

    Talvez finalizado

--- cpp09/ex02/src/class/PMergeMe.cpp
+++ cpp09/ex02/src/class/PMergeMe.cpp
@@ -17,7 +17,7 @@ PMergeMe &PMergeMe::operator=(PMergeMe const &other)
 {
 	if (this != &other)
 	{
-		_list = other._list;
+		_vector = other._vector;
 		_deque = other._deque;
 	}
 	return *this;
@@ -54,12 +54,45 @@ void	PMergeMe::putValues(char **av)
 			throw std::invalid_argument("Error. Negative number.");
 		if (value > INT_MAX)
 			throw std::invalid_argument("Error. Number too big.");
-		_list.push_back(value);
-		_deque.push_back(value);
+		std::vector<int>::iterator it = std::find(this->_vector.begin(), this->_vector.end(), value);
+		if (it != this->_vector.end())
+			throw std::invalid_argument("Error. Duplicate value.");
+		this->_vector.push_back(value);
+		this->_deque.push_back(value);
 	}
 }
 
 void	PMergeMe::run(char **av)
 {
 	putValues(av + 1);
+	std::cout << COLOR_TITLE << "Before: " << COLOR_RESET << std::endl;
+	printContainer(this->_deque);
+
+	// Merging deque
+	clock_t startDeque = std::clock();
+	mergeInsertionSort(this->_deque);
+	clock_t endDeque = std::clock();
+
+	// Merging vector
+	clock_t startVector = std::clock();
+	mergeInsertionSort(this->_vector);
+	clock_t endVector = std::clock();
+
+	std::cout << COLOR_TITLE << "After: " << COLOR_RESET << std::endl;
+	std::cout << COLOR_DEQUE << "deque: " << COLOR_RESET;
+	printContainer(this->_deque);
+	std::cout << COLOR_VECTOR << "vector: " << COLOR_RESET;
+	printContainer(this->_vector);
+
+	double timeElapsedDeque = static_cast<double>(endDeque - startDeque)
+		* 1000000.0 / CLOCKS_PER_SEC;
+	std::cout << "Time for deque: "
+				<< std::fixed << std::setprecision(6)
+				<< timeElapsedDeque << " us" << std::endl;
+
+	double timeElapsedVector = static_cast<double>(endVector - startVector)
+		* 1000000.0 / CLOCKS_PER_SEC;
+	std::cout << "Time for vector: "
+				<< std::fixed << std::setprecision(6)
+				<< timeElapsedVector << " us" << std::endl;
 }
--- cpp09/ex02/src/class/PMergeMe.hpp
+++ cpp09/ex02/src/class/PMergeMe.hpp
@@ -1,23 +1,40 @@
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 
+#define COLOR_DEQUE "\033[1;31m"
+#define COLOR_VECTOR "\033[1;32m"
+#define COLOR_TITLE "\033[1;33m"
+#define COLOR_RESET "\033[0m"
+
 #include <iostream>
 #include <exception>
 #include <string>
-#include <list>
+#include <vector>
 #include <deque>
 #include <algorithm>
 #include <climits>
 #include <cstdio>
+#include <ctime>
+#include <iomanip>
 
 class PMergeMe
 {
     private:
-        std::list<int>  _list;
+        std::vector<int>  _vector;
         std::deque<int> _deque;
 
         bool            isNumber(char *str);
         void            putValues(char **av);
+
+        template <typename TContainer>
+        void    mergeInsertionSort(TContainer &container);
+
+        template <typename TContainer>
+        int     findSmallest(TContainer &container, TContainer &larger);
+
+        template <typename TContainer>
+        void    printContainer(TContainer &container);
+
 
     public:
         PMergeMe();
@@ -27,6 +44,53 @@ class PMergeMe
 
         void    run(char **av);
 };
+
+template <typename TContainer>
+void    PMergeMe::printContainer(TContainer &container)
+{
+    for (typename TContainer::iterator it = container.begin(); it != container.end(); it++)
+        std::cout << *it << " ";
+    std::cout << std::endl;
+}
+
+
+
+template <typename TContainer>
+void PMergeMe::mergeInsertionSort(TContainer &container)
+{
+    size_t size = container.size();
+    if (size < 2)
+        return;
+
+    TContainer larger, smaller;
+    // Divide the container into two parts
+    for (size_t i = 0; i < size; i += 2)
+    {
+        if (i + 1 < size)
+        {
+            larger.push_back(std::max(container[i], container[i + 1]));
+            smaller.push_back(std::min(container[i], container[i + 1]));
+        }
+        else
+            larger.push_back(container[i]);
+    }
+
+    // Recursive call to divide the larger part
+    mergeInsertionSort(larger);
+
+    // Clear the container and copy the sorted larger part
+    container.clear();
+    container = larger;
+
+    // Insert the elements from the smaller part into the sorted container
+    typedef typename TContainer::iterator iterator;
+    for (size_t i = 0; i < smaller.size(); ++i)
+    {
+        iterator insertPos = std::lower_bound(container.begin(), container.end(), smaller[i]);
+        container.insert(insertPos, smaller[i]);
+    }
+}
+
 
 
 #endif
